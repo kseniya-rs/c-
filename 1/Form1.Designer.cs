f1ce3eea71acf21828368ca3f9f685b12e8f720b
kseniya-rs <63047743+kseniya-rs@users.noreply.github.com>
2020-12-15 19:04:35 +0300

    Add files via upload

---
+++ 1/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.PATHBOX = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.FILENAME = new System.Windows.Forms.TextBox();
+            this.rename = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Writter);
+            // 
+            // PATHBOX
+            // 
+            this.PATHBOX.Location = new System.Drawing.Point(72, 89);
+            this.PATHBOX.Name = "PATHBOX";
+            this.PATHBOX.ReadOnly = true;
+            this.PATHBOX.Size = new System.Drawing.Size(540, 20);
+            this.PATHBOX.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Create .txt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Create);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(243, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.FileChoose);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Folder path";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.FolderPick);
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(72, 263);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(540, 82);
+            this.text.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(486, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Delete);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(615, 29);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Copy";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Copy);
+            // 
+            // FILENAME
+            // 
+            this.FILENAME.Location = new System.Drawing.Point(213, 130);
+            this.FILENAME.Name = "FILENAME";
+            this.FILENAME.Size = new System.Drawing.Size(190, 20);
+            this.FILENAME.TabIndex = 9;
+            // 
+            // rename
+            // 
+            this.rename.Location = new System.Drawing.Point(430, 127);
+            this.rename.Name = "rename";
+            this.rename.Size = new System.Drawing.Size(75, 23);
+            this.rename.TabIndex = 10;
+            this.rename.Text = "rename";
+            this.rename.UseVisualStyleBackColor = true;
+            this.rename.Click += new System.EventHandler(this.Rename);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "PATH:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "NAME:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "CONTENT:";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(713, 0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Move";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Move);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rename);
+            this.Controls.Add(this.FILENAME);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.PATHBOX);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox PATHBOX;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox FILENAME;
+        private System.Windows.Forms.Button rename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ 1/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+        private bool folder = false;
+        private bool file = false;
+
+        //СОЗДАНИЕ ФАЙЛА
+        private void Create(object sender, EventArgs e)
+        {
+            if (folder == true && file == false)
+            {
+                string path = PATHBOX.Text;
+                if (Directory.Exists(path) == false)
+                {
+                    MessageBox.Show("ERROR. Check the path or file");
+                }
+                else
+                {
+                    if (FILENAME.Text != null)
+                    {
+                        path += $@"\{FILENAME.Text}.txt";
+                    }
+                    else
+                    {
+                        path += @"\note.txt";
+                    }
+                    using (FileStream fstream = new FileStream($@"{path}", FileMode.Create)) { }
+                    PATHBOX.Text = path;
+                    folder = false;
+                    file = true;
+
+                }
+            }
+            else
+                MessageBox.Show("Choose path");
+        }
+
+        //SAVE
+        private void Writter(object sender, EventArgs e)
+        {
+            if (file == true && folder == false)
+            {
+                string path = PATHBOX.Text;
+                if (File.Exists(path) == false)
+                {
+                    MessageBox.Show("ERROR. Check the path or file");
+                }
+                else
+                {
+                    using (FileStream fstream = new FileStream($@"{path}", FileMode.Open))
+                    {
+                        byte[] array = System.Text.Encoding.Default.GetBytes(text.Text);
+                        fstream.Write(array, 0, array.Length);
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Choose file");
+        }
+        //ВЫБОР ФАЙЛА
+        private void FileChoose(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string path = openFileDialog1.FileName;
+            PATHBOX.Text = path;
+            if (File.Exists(path) == false)
+            {
+                MessageBox.Show("ERROR. Check the path");
+            }
+            else
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    text.Text = sr.ReadToEnd();
+                }
+                FileInfo info = new FileInfo(PATHBOX.Text);
+                FILENAME.Text = info.Name;
+                file = true;
+                folder = false;
+            }
+        }
+
+        //МЕСТО ПОД ФАЙЛ
+        private void FolderPick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                PATHBOX.Text = FBD.SelectedPath;
+                folder = true;
+                file = false;
+            }
+        }
+
+        //Удаление файла
+        private void Delete(object sender, EventArgs e)
+        {
+            if (file == true)
+            {
+                File.Delete(PATHBOX.Text);
+                file = false;
+                PATHBOX.Clear();
+                FILENAME.Clear();
+                text.Text = "";
+            }
+            else MessageBox.Show("file not chosed");
+        }
+        //КОПИРОВАНИЕ
+        private void Copy(object sender, EventArgs e)
+        {
+            if (file == true)
+            {
+                FolderBrowserDialog FBD = new FolderBrowserDialog();
+                if (FBD.ShowDialog() == DialogResult.OK)
+                {
+                    File.Copy(PATHBOX.Text, $@"{FBD.SelectedPath}\NewCopy.txt");
+                }
+            }
+        }
+
+        private void Rename(object sender, EventArgs e)
+        {
+            if (file == true)
+            {
+                FileInfo info = new FileInfo(PATHBOX.Text);
+                string old = info.FullName;
+                string dir = info.DirectoryName;
+                dir += $@"\{FILENAME.Text}";
+                if (old == dir)
+                {
+                    MessageBox.Show("Write new name");
+                    return;
+                }
+                else
+                {
+                    File.Move(old, dir);
+                    PATHBOX.Text = dir;
+                }
+            }
+        }
+
+        private void Move(object sender, EventArgs e)
+        {
+            if (file == true)
+            {
+                FolderBrowserDialog FBD = new FolderBrowserDialog();
+                if (FBD.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo info = new FileInfo(PATHBOX.Text);
+                    string old = info.FullName;
+                    string dir = FBD.SelectedPath;
+                    dir += $@"\{info.Name}";
+                    File.Move(old, dir);
+                    PATHBOX.Text = dir;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Choose file");
+                return;
+            }
+
+        }
+
+    }
+}
